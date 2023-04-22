bff79868644bfdecc6f02754094684960e9aa7ee
rafaelva-z <rafaelvaz.art@gmail.com>
2023-04-22 15:12:22 +0100

    added notes, and conversion function

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,47 +6,52 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 22:02:57 by rvaz              #+#    #+#             */
-/*   Updated: 2023/04/21 18:04:06 by rvaz             ###   ########.fr       */
+/*   Updated: 2023/04/22 15:08:28 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_printf.h"
 
-
-int	ft_printf(char *text, ...)
+static void	ft_conversion(char c, va_list args)
 {
-	long long int		i;
-	va_list	args;
-
-	i = 0;
-	va_start(args, text);
-	while (text[i])
-	{
-		if (text[i] == '%')
-		{	
-		if (text[++i] == 'c')
+	if (c == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
-	else if (text[i] == 's')
+	else if (c == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
-	else if (text[i] == 'p')
-		ft_putnbr_fd(va_arg(args, int), 1); // Missing
-	else if (text[i] == 'd' && text[i] == 'i')
+	else if (c == 'p')
+		ft_putnbr_fd(va_arg(args, unsigned int), 1); // Missing
+	else if (c == 'd' || c == 'i')
 		ft_putnbr_fd(va_arg(args, int), 1);
-	else if (text[i] == 'u')
-		ft_putnbr_fd(va_arg(args, int), 1); // Missing
-	else if (text[i] == 'x')
-		ft_putnbr_fd(va_arg(args, int), 1); // Missing
-	else if (text[i] == 'X')
-		ft_putnbr_fd(va_arg(args, int), 1); // Missing
-	else if (text[i] == '%')
+	else if (c == 'u')
+		ft_putnbr_fd(va_arg(args, unsigned int), 1);
+	else if (c == 'x')
+		ft_putnbr_fd(va_arg(args, unsigned int), 1); // Missing
+	else if (c == 'X')
+		ft_putnbr_fd(va_arg(args, unsigned int), 1); // Missing
+	else if (c == '%')
 		ft_putchar_fd('%', 1);
 	else
 		ft_putchar_fd('%', 1); // If '%' isn't followed by one of these?
-			i++;
-		}
+}
+
+int	ft_printf(char *text, ...)
+{
+	va_list	args;
+	int		i;
+	int		count; // long int?
+
+	i = 0;
+	count = 0;
+	va_start(args, text);
+	if(ft_printf_checker(text, args);
+	while (text[i])
+	{
+		if (text[i] == '%')
+			ft_conversion(text[++i], args);
 		else
-			ft_putchar_fd(text[i++], 1);
+			ft_putchar_fd(text[i], 1);
+		i++;
 	}
-	return (ft_strlen(text));
 	va_end(args);
+	return (count);
 }
