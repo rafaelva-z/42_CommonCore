73275ea0606b324b3e37a0daab507d50277020f7
rafaelva-z <rafaelvaz.art@gmail.com>
2023-04-28 00:18:06 +0100

    get_next_line start

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 20:58:12 by rvaz              #+#    #+#             */
+/*   Updated: 2023/04/28 00:15:10 by rvaz             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 1
+# include <stdlib.h>
+# include <unistd.h>
+
+//Delete the printf library
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
