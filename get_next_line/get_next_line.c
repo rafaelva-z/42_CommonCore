cf8b114bb2276ed9ae40582fed64a5f1f41bf344
rafaelva-z <rafaelvaz.art@gmail.com>
2023-05-01 12:12:47 +0100

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 22:39:26 by rvaz              #+#    #+#             */
-/*   Updated: 2023/04/29 19:39:17 by rvaz             ###   ########.fr       */
+/*   Updated: 2023/04/30 16:03:07 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,28 +31,28 @@ char	*get_next_line(int fd)
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
 
-	i = 0;
+	i = 1;
 	line = NULL;
-
-	read(fd, buffer, BUFFER_SIZE);
-	line = ft_strjoin(line, buffer);
-
 	while (i > 0)
 	{
 		if (!buffer[0])
 			i = read(fd, buffer, BUFFER_SIZE);
 		if (i > 0)
-			line = ft_strjoin(line, buffer);
-		if (check_nl > 0)
+			line = ft_strjoin_nl(line, buffer);
+		if (check_nl(buffer) > 0)
 			break ;
 	}
-	//while()
 	return (line);
 }
 
 int	main()
 {
+	int i;
+
+	i = open("text.txt", O_RDONLY);
 	printf("\nBuff_size: %d\n", BUFFER_SIZE);
-	printf("Buffer: '%s'\n", get_next_line(open("text.txt", O_RDONLY)));
-	printf("Buffer: '%s'\n", get_next_line(open("text.txt", O_RDONLY)));
+	printf("Line: '%s'\n", get_next_line(i));
+	printf("Line: '%s'\n", get_next_line(i));
+	printf("Line: '%s'\n", get_next_line(i));
+	printf("Line: '%s'\n", get_next_line(i));
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 22:39:29 by rvaz              #+#    #+#             */
-/*   Updated: 2023/04/29 12:04:01 by rvaz             ###   ########.fr       */
+/*   Updated: 2023/05/01 09:43:14 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,40 +26,38 @@ size_t	ft_strlen(const char *str)
 {
 	int	i;
 
+	if (!str)
+		return (0);
 	i = 0;
-	while (str[i])
-		i++;
+	while
+		(str[i++]);
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin_nl(char const *s1, char const *s2)
 {
 	char	*new;
 	int		i;
-	int		a;
-
-	if(!s1 && s2)
-		new = malloc(ft_strlen(s2) + 1);
-	else
-		new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	int		j;
+	
+	i = -1;
+	j = -1;
+	if (!s2)
+		return (NULL);
+	new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!new)
 		return (NULL);
-	i = 0;
-	a = 0;
-	if(s1)
+	if (s1)
 	{
-		while (s1[i])
-		{
+		while (s1[++i])
 			new[i] = s1[i];
-			i++;
-		}
 	}
-	while (s2[a])
+	while (s2[++j])
 	{
-		new[i] = s2[a];
-		i++;
-		a++;
+		new[++i] = s2[j];
+		if (s2[j] == '\n')
+			break ;
 	}
-	new[i] = '\0';
+	new[++i] = '\0';
 	return (new);
 }
