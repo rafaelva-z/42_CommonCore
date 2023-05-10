9a2649bd2eb7b732a0a25508ee5233ca9a42162d
rafaelva-z <rafaelvaz.art@gmail.com>
2023-05-10 12:34:11 +0100

    input_check

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,40 +6,62 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:25:43 by rvaz              #+#    #+#             */
-/*   Updated: 2023/05/09 19:53:22 by rvaz             ###   ########.fr       */
+/*   Updated: 2023/05/10 12:22:30 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	ft_error(void)
+static int	ft_error(void)
 {
-	printf("ERROR\n\n");
+	printf("ERROR\n");
+	return (0);
 }
 
-int main(int argc, char **argv)
+int	is_signal(char c)
 {
-	int	i;
-	int	j;
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
+}
+
+int	input_check(char **input)
+{
+	int		i;
+	int		j;
+	long	this_nb;
 
 	i = 0;
-	if(argc > 2)
-	{
-		
-	while (argv[++i])
+	while (input[++i])
 	{
 		j = -1;
-		while (argv[i][++j])
+		if (!is_signal(input[i][++j]) && !ft_isdigit(input[i][j]))
+			return (0);
+		while (input[i][++j])
 		{
-			if (!ft_isdigit(argv[i][j]))
-			{
-				ft_error();
+			if (!ft_isdigit(input[i][j]))
 				return (0);
-			}
 		}
-		printf("number: %d\n", ft_atoi(argv[i]));
+		this_nb = ft_atoi(input[i]);
+		if (this_nb > INT_MAX || this_nb < INT_MIN)
+			return (0);
+		j = i;
+		while (j > 1)
+		{
+			if (ft_atoi(input[--j]) == this_nb)
+				return (0);
+		}
 	}
-	}
-	else
-		ft_error();
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+
+	if (argc < 2 || !input_check(argv))
+		return (ft_error());
+	printf("Hey");
+	//create_stack(argv);
+	// while (i < argc - 1)
+	// 	printf("number: %d\t", seq[i++]);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:18:08 by rvaz              #+#    #+#             */
-/*   Updated: 2023/05/09 18:34:59 by rvaz             ###   ########.fr       */
+/*   Updated: 2023/05/10 12:14:58 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 # include <stdio.h>
 # include <unistd.h>
+# include <limits.h>
+# include "./libft/libft.h"
 
 typedef struct stack{
 	int		number;
 	void	*next;
 }	t_stack;
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
 
 void	push_swap(int *seq, int seq_size);
+int		input_check(char **input);
+int		is_signal(char c);
 
 #endif
