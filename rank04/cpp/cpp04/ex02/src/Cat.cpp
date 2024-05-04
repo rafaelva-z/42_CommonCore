ccdcee9d3ed4bb17d89c14df8f70a14fc320dfe5
rafaelva-z <rafaelvaz.art@gmail.com>
2024-05-04 15:13:12 +0100

    [cpp04] ex02 Cat and Dog had different behaviour

--- rank04/cpp/cpp04/ex02/src/Cat.cpp
+++ rank04/cpp/cpp04/ex02/src/Cat.cpp
@@ -6,20 +6,20 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 18:22:55 by rvaz              #+#    #+#             */
-/*   Updated: 2024/04/29 13:07:11 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/05/04 15:10:36 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
 //	Orthodox Canonical Form
-Cat::Cat() : Animal(), brain(new Brain)
+Cat::Cat() : Animal(), brain(new Brain())
 {
 	std::cout << "Cat Default constructor called." << std::endl;
 	type = "Cat";
 }
 
-Cat::Cat(const Cat &other) : Animal(other)
+Cat::Cat(const Cat &other) : Animal(other), brain(new Brain(other.getBrain()))
 {
 	std::cout << "Cat Copy constructor called." << std::endl;
 	if (this == &other)
@@ -32,7 +32,11 @@ Cat&	Cat::operator=(const Cat& other)
 	std::cout << "Cat Copy Assignment operator called." << std::endl;
 	if (this == &other)
 		return (*this);
+	this->Animal::operator=(other);
+	if (brain)
+		delete brain;
 	type = other.type;
+	brain = new Brain();
 	*brain = *other.brain;
 	return (*this);
 }
