273b75ea2e849c8e5e71b8ad9fe8a161388f8000
rafaelva-z <rafaelvaz.art@gmail.com>
2024-05-20 12:39:10 +0100

    [cpp06] update

--- rank05/cpp/cpp06/ex00/ScalarConverter.cpp
+++ rank05/cpp/cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:11:36 by rvaz              #+#    #+#             */
-/*   Updated: 2024/05/15 18:44:30 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/05/16 16:57:40 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,6 @@ static int	checkNbType(std::string str)
 	long	nb;
 
 	nb = atol(str.c_str());
-	std::cout << nb << std::endl;
 	if (nb <= std::numeric_limits<int>::max() && nb >= std::numeric_limits<int>::min())
 		return (TYPE_INT);
 	if (nb <= std::numeric_limits<long>::max() && nb >= std::numeric_limits<double>::min())
@@ -103,7 +102,7 @@ static int	checkNb(std::string str)
 		i++;
 	if (!isdigit(str[i]))
 	{
-		std::cerr << "error1" << std::endl;
+		std::cerr << "Error: Bad character at the start (index 0 or 1)" << std::endl;
 		return (TYPE_ERROR);
 	}
 	while (isdigit(str[i]))
@@ -115,14 +114,14 @@ static int	checkNb(std::string str)
 		return (TYPE_FLOAT);
 	else if (str[i] == 'f')
 	{
-		std::cerr << "error f out of place" << std::endl;
+		std::cerr << "Error: f out of place" << std::endl;
 		return (TYPE_ERROR);
 	}
 	if (str[i] == '.')
 		i++;
 	if (!isdigit(str[i]))
 	{
-		std::cerr << "error2" << std::endl;
+		std::cerr << "Error: Bad character on string" << std::endl;
 		return (TYPE_ERROR);
 	}
 	while (isdigit(str[i]))
@@ -133,16 +132,15 @@ static int	checkNb(std::string str)
 		return (TYPE_FLOAT);
 	else if (str[i] == 'f')
 	{
-		std::cerr << "error f out of place" << std::endl;
+		std::cerr << "Error: f out of place on second segment" << std::endl;
 		return (TYPE_ERROR);
 	}
-	std::cerr << "end of number" << std::endl;
+	std::cerr << "Error: end of number" << std::endl;
 	return (TYPE_ERROR);
 }
 
 static void printConversionInt(std::string &str)
 {
-	std::cout << "DEBUG::TYPE INT" << std::endl;
 	float value;
 
 	value = atoi(str.c_str());
@@ -160,7 +158,6 @@ static void printConversionInt(std::string &str)
 
 static void printConversionChar(std::string &str)
 {
-	std::cout << "DEBUG::TYPE CHAR" << std::endl;
 	if (isprint(str[0]))
 		std::cout << "char:\t"	<< str[0] << std::endl;
 	else
@@ -172,7 +169,6 @@ static void printConversionChar(std::string &str)
 
 static void printConversionFloat(std::string &str)
 {
-	std::cout << "DEBUG::TYPE FLOAT" << std::endl;
 	float value;
 
 	value = strtof(str.c_str(), NULL);
@@ -198,7 +194,6 @@ static void printConversionFloat(std::string &str)
 
 static void	printConversionDouble(std::string &str)
 {
-	std::cout << "DEBUG::TYPE DOUBLE" << std::endl;
 	float value;
 
 	value = strtod(str.c_str(), NULL);
@@ -215,12 +210,18 @@ static void	printConversionDouble(std::string &str)
 		std::cout << static_cast<int>(value) << std::endl;
 	else
 		std::cout << "impossible" << std::endl;
-	
-	std::cout << "float:\t"		<< static_cast<float>(value) << std::endl;
-	if (value == static_cast<int>(value))
-		std::cout << ".0f" << std::endl;
+	std::cout << "float:\t";
+	if (static_cast<float>(value) <= std::numeric_limits<float>::max()
+		&& static_cast<float>(value) >= std::numeric_limits<float>::min())
+	{
+		std::cout << static_cast<float>(value);
+		if (value == static_cast<int>(value))
+			std::cout << ".0f" << std::endl;
+		else
+			std::cout << "f" << std::endl;
+	}
 	else
-		std::cout << "f" << std::endl;
+		std::cout << BAD_CONVERSION << std::endl;
 	std::cout << "double:\t"	<< value;	
 	if (value == static_cast<int>(value))
 		std::cout << ".0" << std::endl;
@@ -246,11 +247,9 @@ static void	printConversion(int type, std::string &str)
 			printConversionDouble(str);
 			break ;
 		default:
-			std::cout << "DEBUG::TYPE NONE" << std::endl;
 			print_input(type);
 			break;
 	}
-
 }
 
 void	ScalarConverter::convert(std::string str)
@@ -276,7 +275,6 @@ void	ScalarConverter::convert(std::string str)
 		print_input(TYPE_ERROR);
 		return ;
 	}
-	std::cout << "DEBUG::input >" << clean_str << std::endl << std::endl;
 	// check for pseudo literals
 	type = checkPseudoLiterals(clean_str);
 	// check other type
--- rank05/cpp/cpp06/ex00/main.cpp
+++ rank05/cpp/cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvaz <rvaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:59:47 by rvaz              #+#    #+#             */
-/*   Updated: 2024/05/13 18:00:26 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/05/16 17:04:38 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,87 @@
 #include <iostream>
 #include <string>
 
+// Colors for better readability on terminal
+#define CYAN "\033[96m";
+#define RED "\033[0;31m";
+#define BLUE  "\033[1;94m";
+#define RESET "\033[0m";
+
+void	print_info(std::string s)
+{
+	std::cout << std::endl;
+	std::cout << CYAN; 
+	std::cout << "- "<< s;
+	std::cout << RESET
+	std::cout << std::endl;
+}
+
+void	print_header(std::string s)
+{
+	static int head_count;
+	std::cout << std::endl;
+	std::cout << BLUE; 
+	std::cout << "---- " << head_count << " " << s << " ----";
+	std::cout << RESET
+	std::cout << std::endl;
+	head_count++;
+}
+
 int	main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc > 2)
 	{
-		std::cout << "Invalid number of arguments (do ./ScalarConverter <value>)"
-			<< std::endl;
+		std::cout << "Invalid number of arguments (do ./ScalarConverter or"
+			<< " ./ScalarConverter <value>)";
 		return (1);
 	}
-	ScalarConverter::convert(std::string(argv[1]));
+	if (argc == 2)
+		ScalarConverter::convert(std::string(argv[1]));
+	else
+	{
+		print_header("Subject");
+		ScalarConverter::convert(std::string("0"));
+		ScalarConverter::convert(std::string("nan"));
+		ScalarConverter::convert(std::string("*"));
+		std::cin.get();
+
+		print_header("Pseudo");
+		ScalarConverter::convert(std::string("+inf"));
+		ScalarConverter::convert(std::string("+inff"));
+		ScalarConverter::convert(std::string("-inf"));
+		ScalarConverter::convert(std::string("-inff"));
+		ScalarConverter::convert(std::string("nan"));
+		std::cin.get();
+
+		print_header("Chars");
+		ScalarConverter::convert(std::string("c"));
+		ScalarConverter::convert(std::string("A"));
+		ScalarConverter::convert(std::string("_"));
+		ScalarConverter::convert(std::string("!"));
+		std::cin.get();
+		
+		print_header("Int");
+		ScalarConverter::convert(std::string("-42"));
+		ScalarConverter::convert(std::string("42"));
+		ScalarConverter::convert(std::string("2147483647"));
+		ScalarConverter::convert(std::string("-2147483648"));
+		std::cin.get();
+		
+		print_header("Float"); 
+		ScalarConverter::convert(std::string("4.2f"));
+		std::cin.get();
+		
+		print_header("Double");
+		ScalarConverter::convert(std::string("4.321"));
+		std::cin.get();
+
+		print_header("Oher invalid input");
+		ScalarConverter::convert(std::string(""));
+		ScalarConverter::convert(std::string("Hello"));
+		ScalarConverter::convert(std::string("123 42"));
+		ScalarConverter::convert(std::string("123f11"));
+		ScalarConverter::convert(std::string("1.1ff"));
+
+		print_header("End of tests");
+	}
 }
