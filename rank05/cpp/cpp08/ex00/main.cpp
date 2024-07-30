8866e22aeb334f46916bbe118ff890aedf846dbd
rafaelva-z <rafaelvaz.art@gmail.com>
2024-07-30 16:46:00 +0100

    [cpp08] ex00 code refactoring

--- rank05/cpp/cpp08/ex00/main.cpp
+++ rank05/cpp/cpp08/ex00/main.cpp
@@ -3,59 +3,45 @@
 #include <set>
 #include <list>
 
+//	Note: Associative containers automatically sort and organize elements as they are inserted
 
 int main() {
-    std::vector<int> vec;
-    std::list<int> lst;
-    std::set<int> st;
+	int values[] = {3, 422, 86, 42, 1};
+	size_t values_amt = sizeof(values) / sizeof(values[0]);
 
-    vec.push_back(3);
-    vec.push_back(422);
-    vec.push_back(86);
-    vec.push_back(42);
-    vec.push_back(1);
+	std::vector<int> vec(values, values + values_amt);
+	std::list<int> lst(values, values + values_amt);
+	std::set<int> set(values, values + values_amt); // Associative container
+	
+	try {
+		std::cout << "vector >> " << easyfind(vec, 3) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << "list >> " << easyfind(lst, 422) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << "set >> " << easyfind(set, 86) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 
-    lst.push_back(3);
-    lst.push_back(422);
-    lst.push_back(86);
-    lst.push_back(42);
-    lst.push_back(1);
-
-    st.insert(3);
-    st.insert(422);
-    st.insert(86);
-    st.insert(42);
-    st.insert(1);
-    
-    try {
-        std::cout << "vector >> " << easyfind(vec, 3) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        std::cout << "list >> " << easyfind(lst, 422) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        std::cout << "set >> " << easyfind(st, 1) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        std::cout << "vector >> " << easyfind(vec, 999) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        std::cout << "list >> " << easyfind(lst, 999) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        std::cout << "set >> " << easyfind(st, 999) << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+	try {
+		std::cout << "vector >> " << easyfind(vec, 999) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << "list >> " << easyfind(lst, 999) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << "set >> " << easyfind(set, 999) << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 }
